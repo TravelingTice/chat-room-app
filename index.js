3289e270b816077e0dd0c58dad61869177b66917
TravelingTice <matthijs29@live.com>
2020-05-21 16:52:53 +0200

    Complete chat system

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ app.use(express.static('public'));
 const io = socket(server);
 
 io.on('connection', socket => {
-  console.log('made socket connection', socket.id);
+  socket.on('chat', data => {
+    io.sockets.emit('chat', data);
+  });
 });
--- public/main.js
+++ public/main.js
@@ -3,7 +3,23 @@ const socket = io.connect('http://localhost:4000');
 
 // query dom
 
-const btn = document.getElementById('send-button');
+const form = document.getElementById('chat-form');
 const username = document.getElementById('username');
 const message = document.getElementById('message');
 const outputBox = document.getElementById('output');
+
+// handle sending message
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  socket.emit('chat', {
+    message: message.value,
+    username: username.value
+  });
+
+  message.value = '';
+});
+
+// listen for events
+socket.on('chat', data => {
+  outputBox.innerHTML += `<p><strong>${data.username}:</strong> ${data.message}</p>`
+})
