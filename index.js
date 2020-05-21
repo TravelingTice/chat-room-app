61778d5dcf5561f4bde53ecd6ab90ad8c13dadd2
TravelingTice <matthijs29@live.com>
2020-05-21 16:23:56 +0200

    Add public html page, add socket.io server setup

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 
 // app setup
 const app = express();
 
 const server = app.listen(4000, () => {
   console.log('Listening to requests on port 4000');
+});
+
+// use public folder to serve static files
+app.use(express.static('public'));
+
+// socket setup
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('made socket connection');
 });
